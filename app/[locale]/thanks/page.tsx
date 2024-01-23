1e5cd8aa4646d4b048f2d4b027b2b1526852bc4e
Jose <jcampillo1207@wmail.com>
2024-01-23 15:13:47 -0600

    Changes

--- app/[locale]/thanks/page.tsx
+++ app/[locale]/thanks/page.tsx
@@ -7,14 +7,14 @@ import Link from "next/link"
 export default function ThankPage() {
     const t = useTranslations("Tnks")
     return (
-        <section className="w-full h-fit py-20 lg:py-36 flex items-center justify-center flex-col gap-y-7">
-            <div className="aspect-video flex items-center justify-center h-[300px] relative">
+        <section className="w-full h-fit py-10 lg:py-36 flex items-center justify-center flex-col gap-y-2 md:gap-y-7 px-5 md:px-7 lg:px-14">
+            <div className="aspect-video flex items-center justify-center w-full md:w-auto h-[200px] md:h-[300px] relative">
                 <Image src={"/deliver.png"} alt="Imagen" className="object-contain" fill/>
             </div>
-            <Title className="text-start mx-auto">
+            <Title className="text-center mx-auto">
                 {t("title")}
             </Title>
-            <TextPrimary>
+            <TextPrimary className="text-center">
                 {t("text")}
             </TextPrimary>
             <Button variant={"default"} size={"lg"} asChild className="mt-7">
