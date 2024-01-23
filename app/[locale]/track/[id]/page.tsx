b6da7790c2c75cce8736bb6d288084de25b36d65
Jose <jcampillo1207@wmail.com>
2024-01-22 20:02:48 -0600

    Fix: Added dynamic routing (track)

---
+++ app/[locale]/track/[id]/page.tsx
@@ -0,0 +1,40 @@
+import { Section1 } from "../_components/section1";
+import { Section2 } from "../_components/section2";
+import { Section3 } from "../_components/section3";
+import { google } from 'googleapis';
+
+export async function loader({ params }: any) {
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.EMAIL,
+      private_key: process.env.KEY!.replace(/\\n/gm, '\n'),
+    },
+    scopes: ['https://www.googleapis.com/auth/drive']
+  });
+
+  const drive = google.drive({ version: 'v3', auth });
+  const fileId = process.env.DRIVE_ID;
+  const response = await drive.files.get({
+    fileId: fileId,
+    alt: 'media'
+  }, { responseType: 'json' });
+
+  return {
+    props: {
+      data: response.data
+    }
+  };
+}
+
+export default function Tracking({ data }: any) {
+  console.log(data)
+  return (
+    <main className="w-full h-fit flex flex-col gap-y-14 md:gap-y-20 lg:gap-y-28 pb-10 md:pb-14 lg:pb-16">
+      <Section1 />
+      <Section2 />
+      {data &&
+        <Section3 data={data} />
+      }
+    </main>
+  );
+}
--- app/[locale]/track/_components/section2.tsx
+++ app/[locale]/track/_components/section2.tsx
@@ -1,21 +1,22 @@
 "use client";
 
+import { useState } from "react";
 import { OnTitle, TextPrimary, Title } from "@/components/mainstyles/text";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Search } from "lucide-react";
 import { useTranslations } from "next-intl";
-import axios from "axios";
-import { useState, FormEvent } from "react";
+import { useRouter } from "next/navigation";
 
 export const Section2 = () => {
   const t = useTranslations("TrackPageSec2");
+  const router = useRouter();
+  const [orderId, setOrderId] = useState("");
 
-  const [orderID, setOrderId] = useState<string | undefined>();
-  const handlePerformanceReport = async (event: FormEvent) => {
-    event.preventDefault(); 
-    // TODO: /track/:id
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    router.replace(`/track/${orderId}`);
   };
 
   return (
@@ -26,21 +27,25 @@ export const Section2 = () => {
           {t("TextTrack")}
         </TextPrimary>
       </div>
-      <form className="w-full lg:w-fit bg-white drop-shadow-lg h-fit rounded-2xl flex flex-col items-center justify-start p-7 md:p-10 lg:p-14 gap-y-5 md:gap-y-7 mx-auto">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full lg:w-fit bg-white drop-shadow-lg h-fit rounded-2xl flex flex-col items-center justify-start p-7 md:p-10 lg:p-14 gap-y-5 md:gap-y-7 mx-auto"
+      >
         <OnTitle className="text-primary">{t("title")}</OnTitle>
         <div className="w-full h-fit flex flex-col gap-y-2 items-start justify-start">
           <Label htmlFor="order" className="text-primary text-sm md:text-base">
             {t("label")}
           </Label>
-          <Input
-            id="order"
-            onChange={(e) => setOrderId(e.target.value)}
-            placeholder={t("placeholder")}
-            type="text"
-          />
         </div>
+        <Input
+          id="order"
+          placeholder={t("placeholder")}
+          type="text"
+          value={orderId}
+          onChange={(e) => setOrderId(e.target.value)}
+        />
         <Button
-          onClick={handlePerformanceReport}
+          type="submit"
           variant={"secondary"}
           size={"lg"}
           className="flex gap-x-2 w-full text-white bg-[#FF9900] hover:bg-[#FF9900] hover:shadow-md duration-300 ease-in-out"
