2c1a3481ec8a59a1eda6dc247b56117f24125a10
Jose <jcampillo1207@wmail.com>
2024-01-25 09:24:05 -0600

    Changes

--- app/[locale]/cultura/_components/section2.tsx
+++ app/[locale]/cultura/_components/section2.tsx
@@ -1,11 +1,13 @@
 import { Title } from "@/components/mainstyles/text";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 export const Section2 = () => {
+  const t = useTranslations("AboutPageSec2");
   return (
     <section className="w-full h-fit flex flex-col gap-y-10 lg:gap-y-14 items-center justify-center px-5 md:px-7 lg:px-14  pb-10 md:pb-14">
       <Title className="lg:text-center">
-        Equipo de líderes en el transporte
+        {t("title")}
       </Title>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full h-fit gap-x-10 gap-y-7">
         <div className="w-full h-[300px] relative flex items-center justify-center rounded-lg overflow-hidden">
